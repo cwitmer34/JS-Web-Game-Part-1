2a2708da472b16a6a87a4924c06522f2cccd5191
cwitmer34 <connorwitmer34@gmail.com>
2022-06-28 07:13:24 -0400

    pixel game

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(source, left, bottom) {
+  let image = document.createElement("img");
+  image.src = source;
+  image.style.position = "fixed";
+  image.style.left = left + "px";
+  image.style.bottom = bottom + "px";
+  document.body.append(image);
+  return image;
+}
+
+function newItem(name, left, bottom) {
+  let item = newImage(`assets/${name}.png`, left, bottom);
+  item.addEventListener("dblclick", function () {
+    item.remove();
+  });
+}
+
+newImage("assets/green-character.gif", 100, 100); // green character
+newImage("assets/pine-tree.png", 450, 200); // pine tree
+newImage("assets/tree.png", 200, 300); // tree
+newImage("assets/pillar.png", 350, 100); // pillar
+newImage("assets/crate.png", 150, 200); // crate
+newImage("assets/well.png", 500, 425); // well
+newItem(`sword`, 500, 405); // sword
+newItem(`sheild`, 165, 185);
+newItem(`staff`, 600, 100);
